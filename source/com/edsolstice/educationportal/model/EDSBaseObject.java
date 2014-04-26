98c7002aaf35d46375a0c4b4e4c2bfdf40a0e5d7
Pravin <nitinnitinp@yahoo.co.in>
2014-04-26 13:48:07 +0530

    added base class

---
+++ source/com/edsolstice/educationportal/model/EDSBaseObject.java
@@ -0,0 +1,27 @@
+package com.edsolstice.educationportal.model;
+
+public class EDSBaseObject {
+
+	String name;
+	String uid;
+	String id;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getUid() {
+		return uid;
+	}
+	public void setUid(String uid) {
+		this.uid = uid;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+
+}
