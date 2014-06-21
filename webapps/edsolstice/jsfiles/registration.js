ce95aeeeaafc0a96fd388f314f27db166863550f
U-Nitin-PC\Nitin <nitin.patidar@hp.com>
2014-06-21 15:14:26 +0530

    chages GUI layer

--- webapps/edsolstice/jsfiles/registration.js
+++ webapps/edsolstice/jsfiles/registration.js
@@ -1,36 +1,116 @@
 $("#btnRegister").button().bind ("click", function() {
 
-$.ajax( {
-	
-    type: 'POST',
-    url: '/edsolstice/rest/registrationservice/users',
-    contentType:'application/json',
-    async: false,
-     
-    
-    data: JSON.stringify ({ 
-	"userName": $("#fname").val(), 
-    "surName": $("#lname").val(),
-	"password": $("#password").val(), 
-    "cPpassword": $("#cPpassword").val(),
-    "department": $("#department").val(),
-    "college": $("#college").val(),
-    "mobile": $("#mobile").val(),
-    "emailId": $("#emailId").val(),
-    "role": $("#type").val()
-		
-			
+ //  var registration = new Registration();
+  //  registration.init();
+    //registration.validate();
+
+	$.ajax( {
+
+		type: 'POST',
+		url: '/edsolstice/rest/registrationservice/users',
+		contentType:'application/json',
+		async: false,
+
+
+		data: JSON.stringify ({ 
+			"userName": $("#fname").val(), 
+			"surName": $("#lname").val(),
+			"password": $("#password").val(), 
+			"cPpassword": $("#cPpassword").val(),
+			"department": $("#department").val(),
+			"college": $("#college").val(),
+			"mobile": $("#mobile").val(),
+			"emailId": $("#emailId").val(),
+			"role": $("#type").val()
+
+
 		}),
-		
-	dataType: 'json',
-    
-   error: function(jqXHR, textStatus, ex) {
-        alert(textStatus + "," + ex + "," + jqXHR.status);	
-    },
-    
-    success : function(xhr){
-      alert("success");	
-    }
-}
-);
+
+		dataType: 'json',
+
+		error: function(jqXHR, textStatus, ex) {
+			alert(textStatus + "," + ex + "," + jqXHR.status);	
+		},
+
+		success : function(xhr){
+			alert("success");	
+		}
+	}
+	);
 });
+
+
+//$(document).ready(function() {
+//$("#registration").validate({
+//	
+//rules: {
+//	
+//// no quoting necessary
+//mobile: "required"
+//},
+//messages: {
+//	mobile: "Please enter your first name"
+//}
+//});
+//});
+
+$(document).ready(function(){
+	  $("p").click(function(){
+	    $(this).hide();
+	  });
+	});x
+
+//function Registration () {
+//	
+//	this.userName = $("#fname").val();
+//	this.surName = $("#lname").val();
+//	this.password = $("#password").val(); 
+//	this.cPpassword = $("#cPpassword").val();
+//	this.department = $("#department").val();
+//	this.college = $("#college").val();
+//	this.mobile = $("#mobile").val();
+//	this.emailId = $("#emailId").val()
+//	this.role = $("#type").val();
+//
+//	} 
+//
+//
+//Registration.prototype.validate = function () {
+//    	
+//	var nameReg = /^[A-Za-z]+$/;
+//	var numberReg =  /^[0-9]+$/;
+//	var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+//
+//	//$('.error').hide();
+//
+//	if(this.userName == ""){
+//		$('#fname').after('<span class="error"> Please enter your first name</span>');
+//	} 
+//	else if(!nameReg.test(this.userName)){
+//		$('#fname').after('<span class="error"> Letters only</span>');
+//	}
+//
+//	if(this.surName == ""){
+//		$('#lname').after('<span class="error"> Please enter your last name</span>');
+//	} 
+//	else if(!nameReg.test(this.surName)){
+//		$('#lname').after('<span class="error"> Letters only</span>');
+//	}
+//
+//	if(this.emailId == ""){
+//		$('#emailId').after('<span class="error"> Please enter your email </span>');
+//	} 
+//	else if(!emailReg.test(this.emailId)){
+//		$('#emailId').after('<span class="error"> Please enter a valid email address</span>');
+//	}
+//
+//	if(this.mobile == ""){
+//		$('#mobile').after('<span class="error"> Please enter your mobile</span>');
+//	} 
+//	else if(!numberReg.test(this.mobile)){
+//		$('#mobile').after('<span class="error"> Numbers only</span>');
+//	}
+//return false ;
+//
+//}   
+
