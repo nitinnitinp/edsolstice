49e963c45bc101786cb91780bfa98076169f5aa2
Vignesh S <vicky.gct@gmail.com>
2014-09-20 10:12:59 +0530

    Changed the home screen

--- webapps/edsolstice/jsfiles/imageslider.js
+++ webapps/edsolstice/jsfiles/imageslider.js
@@ -13,7 +13,7 @@
 
             // transition valuess
             animtype        : 'fade',
-            animduration    : 450,      // length of transition
+            animduration    : 1000,      // length of transition
             animspeed       : 4000,     // delay between transitions
             automatic       : true,     // enable/disable automatic slide rotation
 
