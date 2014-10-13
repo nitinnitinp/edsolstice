4dcbc7f121be15bbb4f285a80a1eeebafa9fee88
Vignesh S <vicky.gct@gmail.com>
2014-10-14 00:12:45 +0530

    Modified the login screen again

--- webapps/edsolstice/jsfiles/login.js
+++ webapps/edsolstice/jsfiles/login.js
@@ -1,28 +1,76 @@
+	var caption = ['Videos','Knowledge Sharing','Forums','All @ One'];
+	var description = ['Desc About the Video Functionality ',
+						'Desc about knowledge sharing',
+						'Desc about Forums',
+						'Desc about all professsionals at one place'];
+	var animInterval;
 
 $( document ).ready(function() {
 	$(".spinner-2").hide();
-	$("#registrationpanel").hide();
-	$("#linkspanel").hide();
-	$("#contactuspanel").hide();
-	$("#feedbackpanel").hide();
-	$("#aboutuspanel").hide();
+	resetAll();
+	$("#slidePanel").show();
+	$("#loginPanel").show();	
 	
-	
+	slider();	
 });
 
+function resetAll(){
+	$("#content1").children().hide();
+}
+
+
+function slider(){
+		var images=$("#slides").children();	
+		i=0;
+		function run(){			
+			$("#description").fadeOut(1000,function(){
+				$("#slides p").fadeOut(1000,function(){
+					$(images[i]).fadeOut(2000);
+					i++;
+					if(i==4){i=0;}
+					$(images[i]).fadeIn(2000,function(){
+						$("#slides p").text(caption[i]).fadeIn(1000,function(){
+							$("#description").text(description[i]).fadeIn(1000);
+						});
+					});	
+				});
+			});
+			animInterval=setTimeout(run,8500);	
+		}
+		setTimeout(run,3000);
+	}
+
 $("#signupicon").bind ("click", function(){
-	$("#textPanel").hide();
-	$("#loginPanel").hide();
+	resetAll();
 	$("#registrationpanel").show();
 	
 });
 
 $("#aboutusicon").bind ("click", function(){
-	$("#textPanel").hide();
-	$("#loginPanel").hide();
+	resetAll();
 	$("#aboutuspanel").show();
 	
 });
+
+$("#homeicon").bind ("click", function(){
+	resetAll();
+	$("#slidePanel").show();
+	$("#loginPanel").show();	
+	
+});
+
+$("#notificationicon").bind ("click", function(){
+	resetAll();	
+});
+
+$("#feedbackicon").bind ("click", function(){
+	resetAll();	
+	$("#feedbackpanel").show();
+});
+$("#contactusicon").bind ("click", function(){
+	resetAll();	
+});
+
 
 $(".round-button").bind ("click", function(){
 	$(".spinner-2").show();
@@ -54,22 +102,22 @@ $(".round-button").bind ("click", function(){
 //            return false;   
 //});
 
-$("#email").bind ("blur	", function(){
-	if($("#email").val() == ''){
-		$("#emailerrormessage").show();
-		return false;
-	}else{
-		$("#emailerrormessage").hide();
-		return true;
-	}
-});
-
-$("#password").bind ("blur	", function(){
-	if($("#email").val() == ''){
-		$("#passworderrormessage").show();
-		return false;
-	}else{
-		$("#passworderrormessage").hide();
-		return true;
-	}
-});
+//$("#email").bind ("blur	", function(){
+//	if($("#email").val() == ''){
+//		$("#emailerrormessage").show();
+//		return false;
+//	}else{
+//		$("#emailerrormessage").hide();
+//		return true;
+//	}
+//});
+//
+//$("#password").bind ("blur	", function(){
+//	if($("#email").val() == ''){
+//		$("#passworderrormessage").show();
+//		return false;
+//	}else{
+//		$("#passworderrormessage").hide();
+//		return true;
+//	}
+//});
