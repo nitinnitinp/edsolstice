8c86a0fd5fce310962bed55c785405f0a200afb7
unknown <nitinnitinp@yahoo.co.in>
2014-11-16 23:35:48 +0530

    uplload profile pic

--- source/com/edsolstice/educationportal/rest/app/controller/FileUploadRest.java
+++ source/com/edsolstice/educationportal/rest/app/controller/FileUploadRest.java
@@ -4,30 +4,32 @@ import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.edsolstice.educationportal.rest.app.service.FileUploadService;
+
 
 @Controller
 @RequestMapping("/uploadservice")
 public class FileUploadRest {
+	@Autowired
+	FileUploadService fileUploadService;
 	
 	 @RequestMapping(value="/upload", method=RequestMethod.POST, consumes = "multipart/form-data")
-	    public @ResponseBody String handleFileUpload(
+	    public @ResponseBody String upload(
 	            @RequestParam("file") MultipartFile file){
 	        if (!file.isEmpty()) {
 	            try {
 	                byte[] bytes = file.getBytes();
 	                String name=  file.getOriginalFilename();
-	                System.out.println("file name : " +name);
-	                BufferedOutputStream stream =
-	                        new BufferedOutputStream(new FileOutputStream(new File(name)));
-	                stream.write(bytes);
-	                stream.close();
+	                fileUploadService.upload(name , bytes);
 	                return "You successfully uploaded " + name + " into " + name + "-uploaded !";
 	            } catch (Exception e) {
 	                return "You failed to upload " + e.getMessage();
@@ -36,6 +38,26 @@ public class FileUploadRest {
 	            return "You failed to upload  because the file was empty.";
 	        }
 	    }
+	 
+	 
+	 @RequestMapping(value="/profilepic/{uid}", method=RequestMethod.POST, consumes = "multipart/form-data")
+	    public @ResponseBody String uploadProfilePic(
+	    		@PathVariable ("uid") String uid, @RequestParam("file") MultipartFile file){
+	        if (!file.isEmpty()) {
+	            try {
+	                byte[] bytes = file.getBytes();
+	                String name=  file.getOriginalFilename();
+	                fileUploadService.uploadProfilePic(uid, name , bytes);
+	                
+	                return "You successfully uploaded " + name + " into " + name + "-uploaded !";
+	            } catch (Exception e) {
+	                return "You failed to upload " + e.getMessage();
+	            }
+	        } else {
+	            return "You failed to upload  because the file was empty.";
+	        }
+	    }
+
 
 	}
 
--- source/com/edsolstice/educationportal/rest/app/service/FileUploadService.java
+++ source/com/edsolstice/educationportal/rest/app/service/FileUploadService.java
@@ -1,5 +1,39 @@
 package com.edsolstice.educationportal.rest.app.service;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.springframework.stereotype.Service;
+
+import com.edsolstice.educationportal.db.DBFactory;
+import com.edsolstice.educationportal.dbmodel.Student;
+import com.edsolstice.educationportal.exception.EDSExceptionErrorCode;
+import com.edsolstice.educationportal.exception.EDSExceptionMessage;
+import com.edsolstice.educationportal.exception.EDSOperationException;
+
+@Service
 public class FileUploadService {
+
+	public void upload(String name, byte[] bytes) throws IOException {
+		 BufferedOutputStream stream =
+                 new BufferedOutputStream(new FileOutputStream(new File(name)));
+         stream.write(bytes);
+         stream.close();
+		
+	}
+
+	public void uploadProfilePic(String uid, String name, byte[] bytes) throws IOException {
+		
+//		Student student=DBFactory.getStudentDB().get("uid" , uid);
+//	
+//		if(student == null) {
+//			throw new EDSOperationException(EDSExceptionErrorCode.INVALIDINPUTS, EDSExceptionMessage.STUDENTDOESNOTEXIST + " "+uid);	
+//		}
+	
+		upload("./images/profile.jpg", bytes);
+		
+	}
 
 }
